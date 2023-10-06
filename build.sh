9c6df4b84c85d0019c62473cdd432fcd467da493
Thepropollo <111026218+Thepropollo@users.noreply.github.com>
2023-10-05 23:21:19 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -o errexit
 pip install -r requirements.txt
 
 [[ $CREATE_SUPERUSER ]];
+then
+  python rendertasks/manage.py createsuperuser --no-input
+fi
 
 python manage.py collectstatic --no-input
 python manage.py migrate
